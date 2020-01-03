bd359dec3bb0749801eea2fa568bf1b4cbe5fee8
Wade <wade.tait@gmail.com>
2020-01-02 22:51:28 -0600

    Using openzeppelin test helpers.
    import openzeppelin test helper module. describes tutorial TutorialToken
    transfer and passes first case: 'reverts when transferring tokens to the
    zero address'.

--- test/TutorialToken.test.js
+++ test/TutorialToken.test.js
@@ -1,4 +1,10 @@
 const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+const {
+    BN,           // Big Number support
+    constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+  } = require('@openzeppelin/test-helpers');
 const TutorialToken = contract.fromArtifact('TutorialToken');
 
 
@@ -9,9 +15,11 @@ require('chai')
 
 describe('TutorialToken', () => {
     const initialSupply = 12000;
+    const[sender, receiver] = accounts;
 
     beforeEach(async function () {
         this.token = await TutorialToken.new(initialSupply);
+        this.value = new BN(1);
     });
 
     describe('token attributes', function() {
@@ -32,6 +40,15 @@ describe('TutorialToken', () => {
             let totalSupply = await this.token.totalSupply()
             totalSupply = totalSupply.toNumber();
             totalSupply.should.be.equal(initialSupply);
+        });
+    });
+
+    describe('token transfers', function() {
+        it('reverts when transferring tokens to the zero address', async function () {
+            await expectRevert(
+                this.token.transfer(constants.ZERO_ADDRESS, this.value, {from: sender }),
+                'ERC20: transfer to the zero address',
+            );
         });
     });
 });
