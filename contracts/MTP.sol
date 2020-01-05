101a18257ec128bf21c7f1e6ad0bb922229b06e9
Wade <wade.tait@gmail.com>
2020-01-04 20:23:24 -0600

    initialize test file for mtp

--- contracts/MTP.sol
+++ contracts/MTP.sol
@@ -1,9 +1,9 @@
 pragma solidity 0.5.12;
 
-import "@openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
-contract MTPTransfer {
+contract MTP {
 
   /**
  * @dev Details of each transfer
@@ -25,34 +25,35 @@ contract MTPTransfer {
 
 Transfer[] public transactions;
 
+mapping(bytes32 => address) public tokens;
 
 ERC20 public ERC20Interface;
 
 
 
-/**  
- * @dev method that handles transfer of ERC20 tokens to other address 
- * it assumes the calling address has approved this contract * as spender 
- * @param symbol_ identifier mapping to a token contract address 
- * @param to_ beneficiary address 
- * @param amount_ numbers of token to transfer */  
- 
- function transferTokens(bytes32 symbol_, address to_, uint256 amount_) public whenNotPaused{  
-  require(tokens[symbol_] != 0x0);  
-  require(amount_ > 0);  
-  
-  address contract_ = tokens[symbol_];  
-  address from_ = msg.sender;  
-  
-  ERC20Interface = ERC20(contract_);  
-  
-  uint256 transactionId = transactions.push(  
-  Transfer({  
-  contract_:  contract_,  
-            to_: to_,  
-            amount_: amount_,  
-            failed_: true  
-  })  
- );  
+/**
+ * @dev method that handles transfer of ERC20 tokens to other address
+ * it assumes the calling address has approved this contract * as spender
+ * @param symbol_ identifier mapping to a token contract address
+ * @param to_ beneficiary address
+ * @param amount_ numbers of token to transfer */
 
+  function transferTokens(bytes32 symbol_, address to_, uint256 amount_) public {
+  //require(tokens[symbol_] != 0x0);
+  //require(amount_ > 0);
+
+  address contract_ = tokens[symbol_];
+  //address from_ = msg.sender;
+
+  ERC20Interface = ERC20(contract_);
+
+  transactions.push(
+  Transfer({
+  contract_: contract_,
+            to_: to_,
+            amount_: amount_,
+            failed_: true
+  })
+ );
+ }
 }
---
+++ test/MTP.test.js
@@ -0,0 +1,75 @@
+const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+const {
+    BN,           // Big Number support
+    constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+  } = require('@openzeppelin/test-helpers');
+const MTP = contract.fromArtifact('MTP');
+
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
+
+describe('MTP', () => {
+
+    beforeEach(async function() {
+        this.mtp = await MTP.new();
+    })
+
+    describe('MTP Contract', function() {
+        it('tests begin', async function() {
+            const test = true
+            test.should.equal(true);
+        })
+    })
+
+
+})
+
+
+
+
+
+
+    // describe('TutorialToken', () => {
+    //     const initialSupply = 12000;
+    //     //const[sender, receiver] = accounts;
+    
+    //     beforeEach(async function () {
+    //         this.token = await TutorialToken.new(initialSupply);
+    //         // this.value = new BN(1);
+    //     });
+    
+    //     describe('token attributes', function() {
+    //         it('has correct name', async function() {
+    //             const name = await this.token.name();
+    //             name.should.equal("TutorialToken");
+    //         });
+    //         it('has correct symbol', async function() {
+    //             const symbol = await this.token.symbol();
+    //             symbol.should.equal("TT");
+    //         });
+    //         it('has correct decimals', async function() {
+    //             let decimals = await this.token.decimals()
+    //             decimals = decimals.toNumber();
+    //             decimals.should.be.equal(18);
+    //         });
+    //         it('has correct intitialSupply', async function() {
+    //             let totalSupply = await this.token.totalSupply()
+    //             totalSupply = totalSupply.toNumber();
+    //             totalSupply.should.be.equal(initialSupply);
+    //         });
+    //     });
+    
+    //     // describe('token transfers', function() {
+    //     //     it('reverts when transferring tokens to the zero address', async function () {
+    //     //         await expectRevert(
+    //     //             this.token.transfer(constants.ZERO_ADDRESS, this.value, {from: sender }),
+    //     //             'ERC20: transfer to the zero address',
+    //     //         );
+    //     //     });
+    //     // });
+    // });
