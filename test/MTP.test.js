51c7034babef1019156d91218ce0305e433867cf
Wade <wade.tait@gmail.com>
2020-01-25 21:07:45 -0600

    initialize nftoken data before each test

--- test/MTP.test.js
+++ test/MTP.test.js
@@ -16,7 +16,7 @@ describe('MTP', () => {
 
     const MTP = contract.fromArtifact('MTP');
     const TutorialToken = contract.fromArtifact('TutorialToken');
-    const tNFT = contract.fromArtifact('TestNFT');
+    const NFTContract = contract.fromArtifact('TestNFT');
     const initialSupply = 12000;
     const[alice, bob] = accounts;
 
@@ -24,8 +24,6 @@ describe('MTP', () => {
         this.mtp = await MTP.new();
         this.token = await TutorialToken.new(initialSupply, {from: alice});
         this.tokenAddress = await this.token.address;
-        this.nftoken = await tNFT.new();
-        this.nfTokenAddress = await this.nftoken.address;
         this.mtpAddress = await this.mtp.address;
     });
 
@@ -146,16 +144,21 @@ describe('MTP', () => {
         });
     });
 
+    beforeEach(async function() {
+        this.nftokenContract = await NFTContract.new();
+        this.nfTokenAddress = await this.nftokenContract.address;
+        this.nftokenId = new BN('5042');
+        this.nft = await this.nftokenContract.mintUniqueTokenTo(alice, this.nftokenId);
+    });
+
     describe("NFT Transfers", function() {
-        const nftokenId = new BN('5042');
 
         it("should be able to transfer an nft from sender to receiver", async function() {
-            const nft = await this.nftoken.mintUniqueTokenTo(alice, nftokenId);
-            let owner = await this.nftoken.ownerOf(nftokenId);
+            let owner = await this.nftokenContract.ownerOf(this.nftokenId);
             owner.should.equal(alice);
-            await this.nftoken.setApprovalForAll(this.mtpAddress, true, {from: alice});
-            await this.mtp.nfMTPTransfer(this.nfTokenAddress, bob, nftokenId, {from: alice});
-            let newOwner = await this.nftoken.ownerOf(nftokenId);
+            await this.nftokenContract.setApprovalForAll(this.mtpAddress, true, {from: alice});
+            await this.mtp.nfMTPTransfer(this.nfTokenAddress, bob, this.nftokenId, {from: alice});
+            let newOwner = await this.nftokenContract.ownerOf(this.nftokenId);
             newOwner.should.equal(bob);
         });
         it('should have a mapping called nftokens', async function() {
@@ -163,12 +166,11 @@ describe('MTP', () => {
             tokenMapping.should.to.exist;
         });
         it('should add a Token struct to nftokens mapping when a new token is staked', async function() {
-            const nft = await this.nftoken.mintUniqueTokenTo(alice, nftokenId);
-            await this.nftoken.setApprovalForAll(this.mtpAddress, true, {from: alice});
-            await this.mtp.nfMTPTransfer(this.nfTokenAddress, bob, nftokenId, {from: alice});
-            const newTokenStruct = await this.mtp.nftokens.call(nftokenId);
+            await this.nftokenContract.setApprovalForAll(this.mtpAddress, true, {from: alice});
+            await this.mtp.nfMTPTransfer(this.nfTokenAddress, bob, this.nftokenId, {from: alice});
+            const newTokenStruct = await this.mtp.nftokens.call(this.nftokenId);
             const newTokenId = newTokenStruct.token_id_.toNumber();
-            newTokenId.should.equal(nftokenId.toNumber());
+            newTokenId.should.equal(this.nftokenId.toNumber());
         });
     });
 
