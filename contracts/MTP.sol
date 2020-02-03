cb0a18204552b1ac9242d5948757c7ac27507b99
Wade <wade.tait@gmail.com>
2020-02-02 22:11:27 -0600

    creates global balances mapping and index of stakers in token struct

--- contracts/MTP.sol
+++ contracts/MTP.sol
@@ -22,24 +22,27 @@ contract MTP {
     //  }
 
     struct Staker { //change to TokenStaker
-        address staker_Address_;
+        address staker_Address_; //this is redundant. can replace with balances mapping
         //  uint stakerIndex_;
         int staker_Stake_Balance_; //must support negative integers
     }
 
     struct Token {
-        address token_Address_;
+        address token_Address_; //this is redundant
         uint256 token_id_; //for nft tokens
         uint total_Staked_Tokens_;  //update on new stake deposits/withdraws of fungible tokens in/out of mtp network
         uint number_Token_Stakers_;
         uint token_Stake_Balance_;
-        Staker[] token_Stakers_; //mapping doesnt support multiple instances of same staker in the stakechain
+        //Staker[] token_Stakers_; //mapping doesnt support multiple instances of same staker in the stakechain
+        mapping(address => Staker[]) token_Stakers_;
+        mapping(uint256 => address) staker_Index_;
     }
 
 
     mapping(address => Token) public tokens;
     mapping(address => Staker) public stakers;
     mapping(uint256 => Token) public nftokens;
+    mapping(address => int256) public balances;
 
     ERC20 public ERC20Interface;
     IERC721 public ERC721Interface;
@@ -54,38 +57,16 @@ contract MTP {
 
         address from_ = msg.sender;
 
-        // if(stakers[from_].staker_Address_ != from_) {
-        //     addStaker(from_);
-        // }
-
         if(stakers[to_].staker_Address_ != to_) {
             addStaker(to_);
         }
 
-        // if(nftokens[tokenId_].token_id_ != tokenId_) {
-        //     addToken(tokenContract_, from_, to_, tokenId_);
-        // }
-        // else {
-        //     Token storage t = nftokens[tokenId_];
-        //     t.token_Stakers_.push(stakers[to_]);
-        //     t.number_Token_Stakers_ ++;
-        //     t.token_Stake_Balance_ += t.number_Token_Stakers_;  //stakes should be abstracted to external contract or global variable?
-        // }
-
         Token storage t = nftokens[tokenId_];
-        t.token_Stakers_.push(stakers[to_]);
+        t.token_Stakers_[to_].push(stakers[to_]);
+        t.staker_Index_[t.number_Token_Stakers_] = to_;
         t.number_Token_Stakers_ ++;
         t.token_Stake_Balance_ += t.number_Token_Stakers_;
 
-        //update bibo balances
-        // for(uint i = 0; i < t.token_Stakers_.length; i++) {
-        //     address currentStakerAddress = t.token_Stakers_[i].staker_Address_;
-        //     uint stakersBefore = i;
-        //     uint stakersAfter = t.token_Stakers_.length - (i + 1);
-        //     int  stakerNewStakes = int256(stakersAfter) - int256(stakersBefore);
-        //     t.token_Stakers_[i].staker_Stake_Balance_ += stakerNewStakes;
-        //     stakers[currentStakerAddress].staker_Stake_Balance_ += stakerNewStakes;
-        // }
         updateBiboBalances(tokenId_);
 
         ERC721Interface = IERC721(tokenContract_);
@@ -94,50 +75,51 @@ contract MTP {
     }
 
 
-    function mtpTransfer(address token_, address to_, uint256 amount_) public {
-        address from_ = msg.sender;
+    // function mtpTransfer(address token_, address to_, uint256 amount_) public {
+    //     address from_ = msg.sender;
 
-        if(stakers[from_].staker_Address_ != from_) {
-            addStaker(from_);
-        }
+    //     if(stakers[from_].staker_Address_ != from_) {
+    //         addStaker(from_);
+    //     }
 
-        if(stakers[to_].staker_Address_ != to_) {
-            addStaker(to_);
-        }
-
-
-        //Staker storage s = stakers[to_];
-        //s.staker_Address_ = to_; not push stakers when stakers already exist
-
-        if(tokens[token_].token_Address_ != token_) {
-            addToken(token_, from_, to_);
-        } else {
-            Token storage t = tokens[token_];
-            t.token_Stakers_.push(stakers[to_]);
-            t.number_Token_Stakers_ ++;
-            t.token_Stake_Balance_ += t.number_Token_Stakers_;  //stakes should be abstracted to external contract or global variable?
-        }
+    //     if(stakers[to_].staker_Address_ != to_) {
+    //         addStaker(to_);
+    //     }
 
 
-        Token storage t = tokens[token_];
-        t.total_Staked_Tokens_ += amount_;  //this does not equal the circulating supply since
-        //we do not actually index the total number of fungible tokens deposited. but rather #tokens * #transfers
+    //     //Staker storage s = stakers[to_];
+    //     //s.staker_Address_ = to_; not push stakers when stakers already exist
 
-        //update bibo balances
-            for(uint i = 0; i < t.token_Stakers_.length; i++) {
-                address currentStakerAddress = t.token_Stakers_[i].staker_Address_;
-                uint stakersBefore = i;
-                uint stakersAfter = t.token_Stakers_.length - (i + 1);
-                int  stakerNewStakes = int256(stakersAfter) - int256(stakersBefore);
-                t.token_Stakers_[i].staker_Stake_Balance_ += stakerNewStakes;
-                stakers[currentStakerAddress].staker_Stake_Balance_ += stakerNewStakes;
-            }
+    //     if(tokens[token_].token_Address_ != token_) {
+    //         addToken(token_, from_, to_);
+    //     } else {
+    //         Token storage t = tokens[token_];
+    //         t.token_Stakers_[to_].push(stakers[to_]);
+    //         t.number_Token_Stakers_ ++;
+    //         t.token_Stake_Balance_ += t.number_Token_Stakers_;  //stakes should be abstracted to external contract or global variable?
+    //     }
 
-        ERC20Interface = ERC20(token_);
 
-        ERC20Interface.transferFrom(from_, to_, amount_);
-        //emit mtpTransfer
-    }
+    //     Token storage t = tokens[token_];
+    //     t.total_Staked_Tokens_ += amount_;  //this does not equal the circulating supply since
+    //     //we do not actually index the total number of fungible tokens deposited. but rather #tokens * #transfers
+
+    //     //update bibo balances
+    //         for(uint i = 0; i < t.number_Token_Stakers_; i++) {
+    //             address currentStakerAddress = t.staker_Index_[int256(i)];
+    //             uint stakersBefore = i;
+    //             uint stakersAfter = t.number_Token_Stakers_ - (i + 1);
+    //             int256  stakerNewStakes = int256(stakersAfter) - int256(stakersBefore);
+    //             balances[currentStakerAddress] += stakerNewStakes;
+    //             t.token_Stakers_[i].staker_Stake_Balance_ += stakerNewStakes;
+    //             stakers[currentStakerAddress].staker_Stake_Balance_ += stakerNewStakes;
+    //         }
+
+    //     ERC20Interface = ERC20(token_);
+
+    //     ERC20Interface.transferFrom(from_, to_, amount_);
+    //     //emit mtpTransfer
+    // }
 
     //events
     //tokenAdded
@@ -151,47 +133,47 @@ contract MTP {
     );
 
 
-    function addToken(address contractAddress, address tokenSender, address tokenRecipient) public {
-        //numTokens++;
-        Token storage t_ = tokens[contractAddress];
-        t_.token_Address_ = contractAddress;
-        t_.total_Staked_Tokens_ = 0;
-        t_.number_Token_Stakers_ = 2;
-        t_.token_Stake_Balance_ = 1;
-        t_.token_Stakers_.push(stakers[tokenSender]); //tokenRecipient should already be initialized as Staker
-        t_.token_Stakers_.push(stakers[tokenRecipient]);
-        // tokens[contractAddress] = Token(
-        //     {
-        //         token_Address_: contractAddress,
-        //         total_Staked_Tokens_: 0,
-        //         number_Token_Stakers_: 0,
-        //         token_Stake_Balance_: 0,
-        //         token_Stakers_: [Staker(contractAddress, 0)]
-        //     }
-        // );
-        emit TokenAdded(contractAddress);
-    }
+    // function addToken(address contractAddress, address tokenSender, address tokenRecipient) public {
+    //     //numTokens++;
+    //     Token storage t_ = tokens[contractAddress];
+    //     t_.token_Address_ = contractAddress;
+    //     t_.total_Staked_Tokens_ = 0;
+    //     t_.number_Token_Stakers_ = 2;
+    //     t_.token_Stake_Balance_ = 1;
+    //     t_.token_Stakers_.push(stakers[tokenSender]); //tokenRecipient should already be initialized as Staker
+    //     t_.token_Stakers_.push(stakers[tokenRecipient]);
+    //     // tokens[contractAddress] = Token(
+    //     //     {
+    //     //         token_Address_: contractAddress,
+    //     //         total_Staked_Tokens_: 0,
+    //     //         number_Token_Stakers_: 0,
+    //     //         token_Stake_Balance_: 0,
+    //     //         token_Stakers_: [Staker(contractAddress, 0)]
+    //     //     }
+    //     // );
+    //     emit TokenAdded(contractAddress);
+    // }
 
-    function addToken(address contractAddress, address tokenSender, address tokenRecipient, uint256 tokenId) public {
-        Token storage t_ = nftokens[tokenId];
-        t_.token_Address_ = contractAddress;
-        t_.token_id_ = tokenId;
-        //t_.total_Staked_Tokens_ = 0;
-        t_.number_Token_Stakers_ = 2;
-        t_.token_Stake_Balance_ = 1;
-        t_.token_Stakers_.push(stakers[tokenSender]); //tokenRecipient should already be initialized as Staker
-        t_.token_Stakers_.push(stakers[tokenRecipient]);
-        // tokens[contractAddress] = Token(
-        //     {
-        //         token_Address_: contractAddress,
-        //         total_Staked_Tokens_: 0,
-        //         number_Token_Stakers_: 0,
-        //         token_Stake_Balance_: 0,
-        //         token_Stakers_: [Staker(contractAddress, 0)]
-        //     }
-        // );
-        emit TokenAdded(contractAddress);
-    }
+    // function addToken(address contractAddress, address tokenSender, address tokenRecipient, uint256 tokenId) public {
+    //     Token storage t_ = nftokens[tokenId];
+    //     t_.token_Address_ = contractAddress;
+    //     t_.token_id_ = tokenId;
+    //     //t_.total_Staked_Tokens_ = 0;
+    //     t_.number_Token_Stakers_ = 2;
+    //     t_.token_Stake_Balance_ = 1;
+    //     t_.token_Stakers_.push(stakers[tokenSender]); //tokenRecipient should already be initialized as Staker
+    //     t_.token_Stakers_.push(stakers[tokenRecipient]);
+    //     // tokens[contractAddress] = Token(
+    //     //     {
+    //     //         token_Address_: contractAddress,
+    //     //         total_Staked_Tokens_: 0,
+    //     //         number_Token_Stakers_: 0,
+    //     //         token_Stake_Balance_: 0,
+    //     //         token_Stakers_: [Staker(contractAddress, 0)]
+    //     //     }
+    //     // );
+    //     emit TokenAdded(contractAddress);
+    // }
 
     function depositNonFungibleToken(address contractAddress, address tokenOwner, uint256 tokenId) public {
         if(stakers[tokenOwner].staker_Address_ != tokenOwner) {
@@ -203,10 +185,12 @@ contract MTP {
         Token storage t_ = nftokens[tokenId];
         t_.token_Address_ = contractAddress;
         t_.token_id_ = tokenId;
-        Staker storage owner_ = stakers[tokenOwner];
-        Staker storage tokenContract_ = stakers[contractAddress];
-        t_.token_Stakers_.push(tokenContract_);
-        t_.token_Stakers_.push(owner_); //tokenRecipient should already be initialized as Staker
+        Staker  memory owner_ = stakers[tokenOwner];
+        Staker memory tokenContract_ = stakers[contractAddress];
+        t_.token_Stakers_[contractAddress].push(tokenContract_);
+        t_.staker_Index_[0] = contractAddress;
+        t_.token_Stakers_[tokenOwner].push(owner_); //tokenRecipient should already be initialized as Staker
+        t_.staker_Index_[1] = tokenOwner;
         t_.number_Token_Stakers_ = 2; //setting to 2 includes token address as root staker account and depositor/owner as second staker account
         t_.token_Stake_Balance_ = 1;
         //emit TokenAdded(contractAddress);
@@ -216,12 +200,13 @@ contract MTP {
     function updateBiboBalances(uint256 tokenId) private {
         Token storage t_ = nftokens[tokenId];
 
-        for(uint i = 0; i < t_.token_Stakers_.length; i++) {  // i initialized to 1 to skip over token address which is included in
-            address currentStakerAddress = t_.token_Stakers_[i].staker_Address_;
+        for(uint i = 0; i < t_.number_Token_Stakers_; i++) {  // i initialized to 1 to skip over token address which is included in
+            address currentStakerAddress = t_.staker_Index_[i];
             uint stakersBefore = i;
-            uint stakersAfter = t_.token_Stakers_.length - (i + 1);
+            uint stakersAfter = t_.number_Token_Stakers_ - (i + 1);
             int  stakerNewStakes = int256(stakersAfter) - int256(stakersBefore);
-            t_.token_Stakers_[i].staker_Stake_Balance_ += stakerNewStakes;
+           // t_.token_Stakers_[currentStakerAddress].staker_Stake_Balance_ += stakerNewStakes;
+            balances[currentStakerAddress] += stakerNewStakes;
             stakers[currentStakerAddress].staker_Stake_Balance_ += stakerNewStakes;
         }
     }
