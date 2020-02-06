2a5c24c6d85b952f3a2f600136f287a4558d5269
Wade <wade.tait@gmail.com>
2020-02-06 11:57:53 -0600

    remove unused files and rename contracts

--- migrations/2_deploy_token.js
+++
@@ -1,6 +0,0 @@
-const TutorialToken = artifacts.require("./TutorialToken.sol");
-
-module.exports = function(deployer) { 
-    const initialSupply = 12000
-    deployer.deploy(TutorialToken, initialSupply)
-};
