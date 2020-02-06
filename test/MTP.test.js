f8951ce77ed10880a22c10b5092613423831a13c
Wade <wade.tait@gmail.com>
2020-02-06 10:06:18 -0600

    remove erc20 tests for now

--- test/MTP.test.js
+++ test/MTP.test.js
@@ -15,134 +15,9 @@ require('chai')
 describe('MTP', () => {
 
     const MTP = contract.fromArtifact('MTP');
-    const TutorialToken = contract.fromArtifact('TutorialToken');
     const NFTContract = contract.fromArtifact('TestNFT');
-    const initialSupply = 12000;
     const[alice, bob] = accounts;
 
-    // beforeEach(async function() {
-    //     this.mtp = await MTP.new();
-    //     this.token = await TutorialToken.new(initialSupply, {from: alice});
-    //     this.tokenAddress = await this.token.address;
-    //     this.mtpAddress = await this.mtp.address;
-    // });
-
-    // describe('MTP Contract', function() {
-    //     it('tests begin', function() {
-    //         const test = typeof(this.mtp);
-    //         test.should.equal('object');
-    //     });
-    //     it('distributes correct amount to accounts', async function() {
-    //         let founderBalance = await this.token.balanceOf(alice);
-    //         founderBalance = founderBalance.toNumber();
-    //         founderBalance.should.equal(initialSupply);
-    //     });
-    //     it('should be able to  transfer sender token to another wallet', async function() {
-    //         this.value = new BN(1);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: alice});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, bob, this.value, {from: alice})
-    //         let balance = ((await  this.token.balanceOf(bob)).toString());
-    //         balance.should.equal(this.value.toString())
-    //     });
-    //     it('should emit a tokenAdded event when a new token is deposited', async function() {
-    //         this.value = new BN(1);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: alice});
-    //         const receipt = await this.mtp.mtpTransfer(
-    //             this.tokenAddress, bob, this.value, {from: alice}
-    //         );
-
-    //         expectEvent(receipt, 'TokenAdded', {
-    //             _tokenContract: this.tokenAddress
-    //         });
-
-    //         });
-    //     it('should have a mapping called tokens', async function() {
-    //         const tokenMapping = await this.mtp.tokens;
-    //         tokenMapping.should.to.exist;
-    //     });
-    //     it('should add a Token struct to tokens mapping when a new token is staked', async function() {
-    //         this.value = new BN(1);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: alice});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, bob, this.value, {from: alice});
-    //         const newTokenStruct = await this.mtp.tokens.call(this.tokenAddress);
-    //         const stakerCount = await newTokenStruct.number_Token_Stakers_.toNumber();
-    //         newTokenStruct.should.own.include({token_Address_: this.tokenAddress});
-    //         stakerCount.should.equal(2);
-    //     });
-    //     it('should add a Staker struct to stakers mapping when a new staker receives a token', async function() {
-    //         this.value = new BN(1);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: alice});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, bob, this.value, {from: alice});
-    //         const newStakerFromStruct = await this.mtp.stakers.call(alice);
-    //         const stakerFromBalance = await newStakerFromStruct.staker_Stake_Balance_.toNumber();
-    //         const newStakerToStruct = await this.mtp.stakers.call(bob);
-    //         const stakerToBalance = await newStakerToStruct.staker_Stake_Balance_.toNumber();
-    //         newStakerFromStruct.should.own.include({staker_Address_: alice});
-    //         stakerFromBalance.should.equal(1);
-    //         newStakerToStruct.should.own.include({staker_Address_: bob});
-    //         stakerToBalance.should.equal(-1);        
-    //     });
-    //     it('should update staker balances upon new transfers', async function() {
-    //         var account;
-    //         this.mtp = await MTP.new();
-    //         this.token = await TutorialToken.new(initialSupply, {from: alice});
-    //         this.tokenAddress = await this.token.address;
-    //         this.mtpAddress = await this.mtp.address;
-    //         this.value = new BN(1);
-    //         var receiverIndex = 1;
-    //         await this.token.approve(this.mtpAddress, this.value,{from: accounts[0]});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, accounts[1], this.value, {from: accounts[0]});
-    //         var firstStaker = await this.mtp.stakers.call(accounts[1]);
-    //         var firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-    //         console.log(firstBalance);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: accounts[1]});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, accounts[2], this.value, {from: accounts[1]});
-    //         firstStaker = await this.mtp.stakers.call(accounts[2]);
-    //         firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-    //         console.log(firstBalance);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: accounts[2]});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, accounts[3], this.value, {from: accounts[2]});
-    //         firstStaker = await this.mtp.stakers.call(accounts[3]);
-    //         firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-    //         console.log(firstBalance);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: accounts[3]});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, accounts[4], this.value, {from: accounts[3]});
-    //         firstStaker = await this.mtp.stakers.call(accounts[4]);
-    //         firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-    //         console.log(firstBalance);
-    //         await this.token.approve(this.mtpAddress, this.value,{from: accounts[4]});
-    //         await this.mtp.mtpTransfer(this.tokenAddress, accounts[5], this.value, {from: accounts[4]});
-    //         firstStaker = await this.mtp.stakers.call(accounts[4]);
-    //         firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-    //         console.log(firstBalance);
-    //         //const balances = [];
-    //         //const firstStaker = await this.mtp.stakers.call(accounts[0]);
-    //         //const firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-    //         const tokenFirstU = await this.mtp.tokens.call(this.tokenAddress);
-    //         //console.log(tokenFirstU);
-    //         //console.log(accounts);
-    //         //console.log(firstStaker);
-    //         //const stakerToBalance = await newStakerToStruct.staker_Stake_Balance_.toNumber()
-    //         // for ( account of accounts) {
-    //         //     console.log(account);
-    //         //     console.log(accounts[receiverIndex]);
-    //         //     let sender = account;
-    //         //     let receiver = await accounts[receiverIndex];
-    //         //     await this.token.approve(this.mtpAddress, this.value,{from: sender});
-    //         //     await this.mtp.mtpTransfer(this.tokenAddress, receiver, this.value, {from: sender});
-    //         //     receiverIndex ++;
-    //         // }
-    //         //accounts.forEach(async function(account, index) {
-    //             //this.mtp = await MTP.new();
-    //             //this.token = await TutorialToken.new(initialSupply, {from: alice})
-    //             //this.value = new BN(1);
-    //             //console.log(this.token);
-    //             //console.log(index + " - " + account)
-    //             //await this.token.approve(this.mtpAddress, this.value,{from: account});
-    //             //await this.mtp.mtpTransfer(this.tokenAddress, accounts[index + 1], this.value, {from: account});
-    //         //});
-    //     });
-    // });
 
     beforeEach(async function() {
         this.mtp = await MTP.new();
@@ -177,52 +52,36 @@ describe('MTP', () => {
         });
         it('should update staker balances upon new transfers',async function() {
             let stakeChains = [];
-            let tokenBeforeTransfer = await this.mtp.nftokens.call(this.nftokenId);
-            stakeChains.push([tokenBeforeTransfer.token_Stakers_]);
             for(i = 0; i < accounts.length - 1; i++) {
                 let sender = accounts[i];
                 let receiver = accounts[i + 1];
                 await this.nftokenContract.setApprovalForAll(this.mtpAddress, true, {from: sender});
                 await this.mtp.nfMTPTransfer(this.nfTokenAddress, receiver, this.nftokenId, {from: sender});
-                let tokenAfterTransfer = await this.mtp.nftokens.call(this.nftokenId);
-                //let tokenStakers = tokenAfterTransfer;
-                console.log(tokenAfterTransfer);
-                //tokenBiboBal = tokenAfterTransfer.token_Stake_Balance_.toNumber();
                 let stakeChain = [];
-                //console.log(stakeChainObj);
+
                 let numStakers = await this.mtp.getStakeChainLength.call(this.nftokenId);
-                console.log("numStakers: " + numStakers);
-                //let tokenAccount = {"tokenID": null, "tokenBalance": tokenBiboBal};
-                //tokenAccount.tokenID = this.nftokenId;
-                //stakeChain.push(tokenAccou
                 let n = 0;
                 while(n < numStakers) {
                     let currentStakerAddress = await this.mtp.stakeChains.call(this.nftokenId, n);
                     stakeChain.push(currentStakerAddress);
-                    console.log("current staker address: " + currentStakerAddress + " at index: " + n);
                     n++;
                 }
                 stakeChains.push(stakeChain);
-                //console.log("number of stakers after transfer = " + numStakers + ", token Bibo balance = " + tokenBiboBal);
             }
-            console.log(stakeChains);
-            // let biboBalances = [];
-            // //let tokenState = await this.mtp.nftokens.call(this.nftokenId);
-            // //console.log(tokenState);
-            // for(i = 0; i < accounts.length; i++) {
-            //     let stakerAddress = accounts[i];
-            //     let staker = await this.mtp.stakers.call(stakerAddress);
-            //     let biboBalance = staker.staker_Stake_Balance_.toNumber();
-            //     biboBalances.push({"address": stakerAddress, 'biboBalance' : biboBalance });
-            // }
-            // console.log("final balances : ", biboBalances);
-            // let totalSupply = biboBalances.reduce(function(supply, stakerBalance) {
-            //     return supply + stakerBalance.biboBalance;
-            // }, 0);
-            // console.log(totalSupply);
-            // firstStaker = await this.mtp.stakers.call(accounts[0]);
-            // firstBalance = await firstStaker.staker_Stake_Balance_.toNumber();
-            // console.log(firstBalance);
+
+            let lastChain = stakeChains[stakeChains.length - 1];
+            console.log("lastChain: " + lastChain);
+            for(i = 0; i < lastChain.length; i++) {
+                let address = lastChain[i];
+                let balance = await this.mtp.balances.call(address);
+                lastChain[i] = {"address": address, "balance": balance.toNumber()};
+            }
+            console.log(lastChain);
+            let biboTotalSupply = lastChain.reduce(function(total, account) {
+                total += account.balance;
+                return total;
+            }, 0);
+            console.log(biboTotalSupply);
         });
     });
 
